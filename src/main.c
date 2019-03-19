76b8a6c3c7cf15e76645eb82caee43599a9b1637
SnapEye <pisoj61@gmail.com>
2019-03-19 17:24:57 +0100

    Added CMakeList, source files and Mqtt library

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "MQTTClient.h"
+
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+
+ int main(){
+	 //printf( "ISPIS");
+
+	 printf("%d ISPIS", ANSI_COLOR_RED);
+}
+
+void init(){
+
+
+
+}
+
+#endif
